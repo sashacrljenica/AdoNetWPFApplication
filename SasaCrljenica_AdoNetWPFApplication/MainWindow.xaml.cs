1299a30b71a507c390db413754da6e53a6f77587
sashacrljenica <sasha.crljenica@gmail.com>
2019-09-26 21:38:15 +0200

    test

--- SasaCrljenica_AdoNetWPFApplication/MainWindow.xaml.cs
+++ SasaCrljenica_AdoNetWPFApplication/MainWindow.xaml.cs
@@ -16,6 +16,7 @@ using System.Windows.Shapes;
 
 namespace SasaCrljenica_AdoNetWPFApplication
 {
+    //this is test!
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
